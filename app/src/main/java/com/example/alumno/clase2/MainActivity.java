919e6739eae91f2563925a5abab5ee5051064056
alumno <alumno>
2018-04-05 22:00:14 -0300

    Alinear elementos en pantalla.

---
+++ app/src/main/java/com/example/alumno/clase2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.alumno.clase2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+        TextView txt = (TextView)(super.findViewById(R.id.texto));
+        Button btn = (Button)(super.findViewById(R.id.botonsito));
+        EditText edt = (EditText)(super.findViewById(R.id.edt));
+
+        btn.setOnClickListener(new MyListener(txt,edt));
+*/
+    }
+}
---
+++ app/src/main/java/com/example/alumno/clase2/MyListener.java
@@ -0,0 +1,35 @@
+package com.example.alumno.clase2;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by alumno on 05/04/2018.
+ */
+/*
+Implemento una interfaz de view on click listener y el metodo de la lamparita.
+ */
+    /*
+    public class MyListener implements View.OnClickListener{
+
+        private TextView tv;
+        private EditText edt;
+        public MyListener(TextView newtv, EditText newedt)
+        {
+            this.tv = newtv;
+            this.edt = newedt;
+        }
+
+        //Este metodo va a escuchar el boton onclick.
+        @Override
+        public void onClick(View v) {
+            Button bnt = (Button)(v.findViewById(R.id.botonsito));
+            bnt.setText(this.edt.getText());
+            Log.d("Click","Se hizo click");
+        }
+
+}
+*/
